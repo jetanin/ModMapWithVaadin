f4c146f9caed40d2c54d8c6c600b3d8804d782fb
jetanin <jetaninnaitho@gmail.com>
2025-04-30 03:37:47 +0700

    add start point

--- src/main/frontend/global.d.ts
+++ src/main/frontend/global.d.ts
@@ -3,7 +3,7 @@ declare module "*.jpg" {
     export default value;
 }
 
-declare module "./SVJ.jsx"{
+declare module "./Svg.jsx"{
     const SvgIcon: React.FC<React.SVGProps<SVGSVGElement>>;
     export { SvgIcon };
 }
